TinaZhangZW/qemu

commit 24f82e153f9f400f840298fc5c68bb162de40740

virtio-iommu: Add multiqueue support
Having a single queue is a major bottleneck on the guest side since
vCPUs spend a lot of time spinning on the queue lock. Allow
instantiating multiple virtqueue pairs and improve SMP performance.
For now it is an optional num-queues parameter.

Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

hw/virtio/virtio-iommu.c
@@ -983,6 +983,7 @@ static void virtio_iommu_get_config(VirtIODevice *vdev, uint8_t *config_data)
     out_config->domain_range.start = cpu_to_le32(dev_config->domain_range.start);
     out_config->domain_range.end = cpu_to_le32(dev_config->domain_range.end);
     out_config->probe_size = cpu_to_le32(dev_config->probe_size);
+    out_config->num_queues = cpu_to_le16(dev_config->num_queues);
     out_config->bypass = dev_config->bypass;
 
     trace_virtio_iommu_get_config(dev_config->page_size_mask,
@@ -991,6 +992,7 @@ static void virtio_iommu_get_config(VirtIODevice *vdev, uint8_t *config_data)
                                   dev_config->domain_range.start,
                                   dev_config->domain_range.end,
                                   dev_config->probe_size,
+                                  dev_config->num_queues,
                                   dev_config->bypass);
 }
 
@@ -1176,14 +1178,30 @@ static void virtio_iommu_device_realize(DeviceState *dev, Error **errp)
 {
     VirtIODevice *vdev = VIRTIO_DEVICE(dev);
     VirtIOIOMMU *s = VIRTIO_IOMMU(dev);
+    VirtQueue *vq;
+    int i;
 
     virtio_init(vdev, VIRTIO_ID_IOMMU, sizeof(struct virtio_iommu_config));
 
     memset(s->iommu_pcibus_by_bus_num, 0, sizeof(s->iommu_pcibus_by_bus_num));
 
-    s->req_vq = virtio_add_queue(vdev, VIOMMU_DEFAULT_QUEUE_SIZE,
-                             virtio_iommu_handle_command);
-    s->event_vq = virtio_add_queue(vdev, VIOMMU_DEFAULT_QUEUE_SIZE, NULL);
+    if (!s->num_queues) {
+        error_setg(errp, "num-queues property must be larger than 0");
+    }
+
+    for (i = 0; i < s->num_queues; i++) {
+        virtio_add_queue(vdev, VIOMMU_DEFAULT_QUEUE_SIZE,
+                         virtio_iommu_handle_command);
+        /*
+         * For the moment we don't care about multiple event queues, we'll just
+         * use the first one. As events become more important with page faults
+         * we might need some efficiency there. Additional queues do need to be
+         * operational since we're advertising them to the guest.
+         */
+        vq = virtio_add_queue(vdev, VIOMMU_DEFAULT_QUEUE_SIZE, NULL);
+        if (!s->event_vq)
+            s->event_vq = vq;
+    }
 
     /*
      * config.bypass is needed to get initial address space early, such as
@@ -1194,6 +1212,7 @@ static void virtio_iommu_device_realize(DeviceState *dev, Error **errp)
     s->config.input_range.end = UINT64_MAX;
     s->config.domain_range.end = UINT32_MAX;
     s->config.probe_size = VIOMMU_PROBE_SIZE;
+    s->config.num_queues = cpu_to_le16(s->num_queues);
 
     virtio_add_feature(&s->features, VIRTIO_RING_F_EVENT_IDX);
     virtio_add_feature(&s->features, VIRTIO_RING_F_INDIRECT_DESC);
@@ -1203,9 +1222,7 @@ static void virtio_iommu_device_realize(DeviceState *dev, Error **errp)
     virtio_add_feature(&s->features, VIRTIO_IOMMU_F_MAP_UNMAP);
     virtio_add_feature(&s->features, VIRTIO_IOMMU_F_MMIO);
     virtio_add_feature(&s->features, VIRTIO_IOMMU_F_PROBE);
-    if (s->bypass_feature) {
-        virtio_add_feature(&s->features, VIRTIO_IOMMU_F_BYPASS_CONFIG);
-    }
+    virtio_add_feature(&s->features, VIRTIO_IOMMU_F_MQ);
 
     qemu_rec_mutex_init(&s->mutex);
 
@@ -1227,6 +1244,7 @@ static void virtio_iommu_device_unrealize(DeviceState *dev)
 {
     VirtIODevice *vdev = VIRTIO_DEVICE(dev);
     VirtIOIOMMU *s = VIRTIO_IOMMU(dev);
+    int i;
 
     qemu_unregister_reset(virtio_iommu_system_reset, s);
     qemu_remove_machine_init_done_notifier(&s->machine_done);
@@ -1241,8 +1259,9 @@ static void virtio_iommu_device_unrealize(DeviceState *dev)
 
     qemu_rec_mutex_destroy(&s->mutex);
 
-    virtio_delete_queue(s->req_vq);
-    virtio_delete_queue(s->event_vq);
+    for (i = 0; i < 2 * s->num_queues; i++) {
+        virtio_del_queue(vdev, i);
+    }
     virtio_cleanup(vdev);
 }
 
@@ -1402,6 +1421,7 @@ static Property virtio_iommu_properties[] = {
                      TYPE_PCI_BUS, PCIBus *),
     DEFINE_PROP_BOOL("boot-bypass", VirtIOIOMMU, boot_bypass, true),
     DEFINE_PROP_BOOL("bypass-feature", VirtIOIOMMU, bypass_feature, true),
+    DEFINE_PROP_UINT16("num-queues", VirtIOIOMMU, num_queues, 1),
     DEFINE_PROP_END_OF_LIST(),
 };
 

hw/virtio/trace-events
@@ -110,7 +110,7 @@ virtio_iommu_device_reset(void) "reset!"
 virtio_iommu_system_reset(void) "system reset!"
 virtio_iommu_get_features(uint64_t features) "device supports features=0x%"PRIx64
 virtio_iommu_device_status(uint8_t status) "driver status = %d"
-virtio_iommu_get_config(uint64_t page_size_mask, uint64_t start, uint64_t end, uint32_t domain_start, uint32_t domain_end, uint32_t probe_size, uint8_t bypass) "page_size_mask=0x%"PRIx64" input range start=0x%"PRIx64" input range end=0x%"PRIx64" domain range start=%d domain range end=%d probe_size=0x%x bypass=0x%x"
+virtio_iommu_get_config(uint64_t page_size_mask, uint64_t start, uint64_t end, uint32_t domain_start, uint32_t domain_end, uint32_t probe_size, uint16_t num_queues, uint8_t bypass) "page_size_mask=0x%"PRIx64" input range start=0x%"PRIx64" input range end=0x%"PRIx64" domain range start=%d domain range end=%d probe_size=0x%x num_queues=0x%x bypass=0x%x"
 virtio_iommu_set_config(uint8_t bypass) "bypass=0x%x"
 virtio_iommu_attach(uint32_t domain_id, uint32_t ep_id) "domain=%d endpoint=%d"
 virtio_iommu_detach(uint32_t domain_id, uint32_t ep_id) "domain=%d endpoint=%d"

include/hw/virtio/virtio-iommu.h
@@ -48,7 +48,6 @@ typedef struct IOMMUPciBus {
 
 struct VirtIOIOMMU {
     VirtIODevice parent_obj;
-    VirtQueue *req_vq;
     VirtQueue *event_vq;
     struct virtio_iommu_config config;
     uint64_t features;
@@ -64,6 +63,7 @@ struct VirtIOIOMMU {
     bool bypass_feature;
     Notifier machine_done;
     bool granule_frozen;
+    uint16_t num_queues;
 };
 
 #endif

commit 6e63b83a65f39532eec2ebed50bd5e630a029122

hw/virtio/virtio-iommu: Add F_BYPASS_CONFIG feature back
Add VIRTIO_IOMMU_F_BYPASS_CONFIG feature back, which was removed by
commit 24f82e153f9f ("hw/virtio/virtio-iommu: Add multiqueue support").

Fixes: 24f82e153f9f ("hw/virtio/virtio-iommu: Add multiqueue support")
Signed-off-by: Tina Zhang <tina.zhang@intel.com>

hw/virtio/virtio-iommu.c
@@ -1512,6 +1512,9 @@ static void virtio_iommu_device_realize(DeviceState *dev, Error **errp)
     virtio_add_feature(&s->features, VIRTIO_IOMMU_F_MAP_UNMAP);
     virtio_add_feature(&s->features, VIRTIO_IOMMU_F_MMIO);
     virtio_add_feature(&s->features, VIRTIO_IOMMU_F_PROBE);
+    if (s->bypass_feature) {
+        virtio_add_feature(&s->features, VIRTIO_IOMMU_F_BYPASS_CONFIG);
+    }
     virtio_add_feature(&s->features, VIRTIO_IOMMU_F_MQ);
     if (s->page_tables) {
         virtio_add_feature(&s->features, VIRTIO_IOMMU_F_ATTACH_TABLE);